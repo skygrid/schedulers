d623b762eb60459a853dce2a41e59c4f9ed4377e
macbook <isaevnextdoor@gmail.com>
2017-12-29 17:16:22 +0300

    fcfs plus tests

--- schedulers/mainscheduler.go
+++ schedulers/mainscheduler.go
@@ -1,12 +1,41 @@
 package scheduler
 
+import (
+	"bytes"
+	"fmt"
+)
+
 type Scheduler interface {
 	Schedule(jobs []ResourceVolume, workers []ResourceVolume) []Decision
 }
+
+func ToString(decisions []Decision) string {
+	var buffer bytes.Buffer
+	for i, d := range decisions {
+		buffer.WriteString(fmt.Sprintf("pair #%s JobIdx %s WorkerIdx %s\n", i, d.JobIdx, d.WorkerIdx))
+	}
+	return buffer.String()
+}
+func (d Decision) Equal(x Decision) bool {
+	return (d.WorkerIdx == x.WorkerIdx) && (d.JobIdx == x.JobIdx)
+}
+
 type MainScheduler struct {
 	Scheduler
 }
 
 func (m MainScheduler) Schedule(jobs []ResourceVolume, workers []ResourceVolume) []Decision {
-	return []Decision{}
+	//fcfs
+	d := []Decision{}
+	for _, j := range jobs {
+		//first fit
+		for i, w := range workers {
+			if (j.TimePeriod <= w.TimePeriod) && (j.CPU <= w.CPU) && (j.GPU <= w.GPU) && (j.RAMmb <= w.RAMmb) {
+				d = append(d, Decision{JobIdx: j.Id, WorkerIdx: w.Id})
+				workers = append(workers[:i], workers[i+1:]...)
+				break;
+			}
+		}
+	}
+	return d
 }
--- schedulers/mainscheduler_test.go
+++ schedulers/mainscheduler_test.go
@@ -2,9 +2,34 @@ package scheduler
 
 import (
 	"testing"
+	"fmt"
 )
 
-func TestJobCRUD(t *testing.T) {
+func checkDecisionsEqual(a []Decision, b []Decision) bool {
+	for i, x := range a {
+		if !x.Equal(b[i]) {
+			return false
+		}
+	}
+	return true
+}
+func TestMainScheduler(t *testing.T) {
 	m := MainScheduler{}
-	rv1:=ResourceVolume{}
+	o1 := Organization{Name: "SHiP", Quota: 100}
+	rv1 := ResourceVolume{CPU: 2, RAMmb: 1, TimePeriod: 40, Owner: &o1, Id: 21}
+	rv2 := ResourceVolume{CPU: 1, RAMmb: 2, TimePeriod: 40, Owner: &o1, Id: 12}
+	jobs := []ResourceVolume{rv1, rv2}
+	worker1 := ResourceVolume{CPU: 1, RAMmb: 2, Id: 12}
+	worker2 := ResourceVolume{CPU: 2, RAMmb: 1, Id: 21}
+
+	workers := []ResourceVolume{worker1, worker2}
+
+	d := m.Schedule(jobs, workers)
+	d_check := []Decision{{JobIdx: 21, WorkerIdx: 21}, {JobIdx: 12, WorkerIdx: 12}}
+
+	fmt.Println(ToString(d))
+
+	if !checkDecisionsEqual(d, d_check) {
+		t.Fail()
+	}
 }
